522752453b450e51436e0d780efeb93bbe41d6aa
Milan Aleksic <milanaleksic@gmail.com>
2018-12-15 17:01:00 +0100

    day 15: graph making

--- day15/main.go
+++ day15/main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/RyanCarrier/dijkstra"
 )
 
 type player struct {
@@ -64,16 +66,115 @@ func main() {
 }
 
 func part1(maxX, maxY int, track []*cell) (int, int) {
-	maxTick := 3
+	maxTick := 4
 	for tick := 0; tick < maxTick; tick++ {
+		fmt.Println("######################### TICK:", tick)
 		for y := 0; y < maxY; y++ {
 			for x := 0; x < maxX; x++ {
-				//cur := track[linear(x, y, maxX, maxY)]
+				cur := track[linear(x, y, maxX, maxY)]
+				if cur.player == nil || cur.player.lastTick >= tick {
+					continue
+				}
+				if newX, newY, ok := deduceNextCell(x, y, maxX, maxY, track); ok {
+					fmt.Printf("%v moves from %d,%d to %d,%d\n", cur.player.plType, x, y, newX, newY)
+					newLoc := track[linear(newX, newY, maxX, maxY)]
+					newLoc.player = cur.player
+					newLoc.player.lastTick = tick
+					cur.player = nil
+				}
 			}
 		}
 		printState(tick, maxY, maxX, track)
 	}
 	return 0, 0
+}
+
+func deduceNextCell(oldX int, oldY int, maxX int, maxY int, track []*cell) (newX, newY int, found bool) {
+	idsOfEnemies := make([]int, 0)
+	oldId := linear(oldX, oldY, maxX, maxY)
+	old := track[oldId]
+	graph := dijkstra.NewGraph()
+	for y := 0; y < maxY; y++ {
+		for x := 0; x < maxX; x++ {
+			id := linear(x, y, maxX, maxY)
+			iter := track[id]
+			if iter.underlying == WALL {
+				continue
+			}
+			if old.player != nil && iter.player != nil && old.player.plType != iter.player.plType {
+				idsOfEnemies = append(idsOfEnemies, id)
+			}
+			//fmt.Printf("Adding vertex %v,%v (%v)\n", x, y, id)
+			graph.AddVertex(id)
+		}
+	}
+	for y := 0; y < maxY; y++ {
+		for x := 0; x < maxX; x++ {
+			id := linear(x, y, maxX, maxY)
+			iter := track[id]
+			if iter.underlying == WALL {
+				continue
+			}
+			maybeReport(x, y-1, maxX, maxY, track, graph, id, 0)
+			maybeReport(x-1, y, maxX, maxY, track, graph, id, 1)
+			maybeReport(x+1, y, maxX, maxY, track, graph, id, 10)
+			maybeReport(x, y+1, maxX, maxY, track, graph, id, 100)
+		}
+	}
+	var bestPath *dijkstra.BestPath = nil
+	for _, idOfEnemy := range idsOfEnemies {
+		path, err := graph.Shortest(oldId, idOfEnemy)
+		if err != nil {
+			log.Fatal("Shortest path couldn't be found", err)
+		}
+		if bestPath == nil {
+			bestPath = &path
+		} else if path.Distance < bestPath.Distance {
+			bestPath = &path
+		} else if path.Distance == bestPath.Distance {
+			if bestPath.Path[0] > path.Path[0] {
+				bestPath = &path
+			}
+		}
+	}
+	if bestPath == nil {
+		fmt.Printf("No path found from %d:%d\n", oldX, oldY)
+		return -1, -1, false
+	}
+	if len(bestPath.Path) == 2 {
+		fmt.Println("Ready to attack!")
+		return -1, -1, false
+	}
+	firstStep := bestPath.Path[1]
+	return firstStep % maxX, firstStep / maxX, true
+}
+
+func maybeReport(x int, y int, maxX int, maxY int, track []*cell, graph *dijkstra.Graph, id int, price int64) {
+	curr := track[id]
+	if neighborId, ok := maybeGet(x, y, maxX, maxY); ok {
+		neighbor := track[neighborId]
+		if neighbor.underlying == EMPTY && curr.underlying == EMPTY {
+			if neighbor.player != nil && curr.player != nil && neighbor.player.plType == curr.player.plType {
+				return
+			}
+			//fmt.Printf("Adding arc %v<->%v (price %d)\n", id, neighborId, price)
+			err := graph.AddArc(id, neighborId, price)
+			if err != nil {
+				log.Fatalf("Could not add arc between %v and %v: %v", neighborId, id, err)
+			}
+			//err = graph.AddArc(id, neighborId, price)
+			//if err != nil {
+			//	log.Fatalf("Could not add arc between %v and %v: %v", id, neighborId, err)
+			//}
+		}
+	}
+}
+
+func maybeGet(x int, y int, maxX int, maxY int) (int, bool) {
+	if x < 0 || y < 0 || x >= maxX || y >= maxY {
+		return -1, false
+	}
+	return linear(x, y, maxX, maxY), true
 }
 
 func deduceCell(cellContents byte) (result *cell) {
@@ -120,7 +221,6 @@ func readAll(file *os.File, list *list.List) {
 }
 
 func printState(tick int, maxY int, maxX int, track []*cell) {
-	fmt.Printf("Tick: %d\n", tick)
 	for y := 0; y < maxY; y++ {
 		for x := 0; x < maxX; x++ {
 			c := track[linear(x, y, maxX, maxY)]
---
+++ go.mod
@@ -0,0 +1,3 @@
+module go.milanaleksic.net/milanaleksic/adventofcode2018
+
+require github.com/RyanCarrier/dijkstra v0.0.0-20180928224145-3fe1cac16289 // indirect
